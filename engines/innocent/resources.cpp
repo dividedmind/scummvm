25df92ec5c539b964106f235e62c4a3655380817
Rafał Rzepecki <divided.mind@gmail.com>
2008-09-04 16:00:10 +0200

    main.dat is read

--- engines/innocent/resources.cpp
+++ engines/innocent/resources.cpp
@@ -1,12 +1,53 @@
 #include "resources.h"
 
+#include "common/file.h"
+#include "common/util.h"
+
+using namespace Common;
+
 namespace Innocent {
 
-Resources::Resources() : _main(new Resources::Main(this)) {
+Resources::Resources() : _main(new MainDat(this)) {
 }
 
 void Resources::load() {
 	_main->load();
+}
+
+void Datafile::load() {
+	File file;
+
+	debug(2, "Resources::DataFile::load()ing %s", filename());
+	if (!file.open(filename()))
+		error("could not open %s", filename());
+
+	readFile(file);
+	file.close();
+	debug(2, "Resources::DataFile::load()ed %s", filename());
+}
+
+MainDat::MainDat(Resources *res) : Datafile(res), _data(0) {}
+
+MainDat::~MainDat() {
+	if (_data)
+		delete[] _data;
+}
+
+void MainDat::readFile(SeekableReadStream &stream) {
+	_dataLen = stream.readUint16LE();
+	debug(1, "length of main data is %d", _dataLen);
+
+	_data = new byte[_dataLen];
+	stream.seek(0);
+	stream.read(_data, _dataLen);
+	descramble();
+
+	stream.read(_footer, kFooterLen);
+}
+
+void MainDat::descramble() {
+	for (int i = 2; i < _dataLen; i++)
+		_data[i] ^= 0x6f;
 }
 
 } // End of namespace Innocent
--- engines/innocent/resources.h
+++ engines/innocent/resources.h
@@ -1,44 +1,55 @@
 #ifndef INNOCENT_RESOURCES_H
 #define INNOCENT_RESOURCES_H
 
-#include "common/util.h"
+#include "common/stream.h"
 
 namespace Innocent {
 
+class MainDat;
+class GraphicsMap;
+
 class Resources {
 public:
-	class Main;
-	class GraphicsMap;
-
 	Resources();
 	void load();
 
 private:
-	class DataFile;
-
-	Main *_main;
+	MainDat *_main;
 	GraphicsMap *_graphicsMap;
+};
 
-	class Datafile {
-	public:
-		Datafile(Resources *resources) : _resources(resources) {}
-		virtual ~Datafile() {}
-
-		virtual void load() { error("loading file %s not implemented", filename()); }
-		virtual const char *filename() const = 0;
-
-	private:
-		Resources *_resources;
-	};
-
+class Datafile {
 public:
-	class Main : public Datafile {
-	public:
-		Main(Resources *resources) : Datafile(resources) {}
+	Datafile(Resources *resources) : _resources(resources) {}
+	virtual ~Datafile() {}
 
-		const char *filename() const { return "iuc_main.dat"; }
+	virtual void load();
+
+	virtual const char *filename() const = 0;
+	virtual void readFile(Common::SeekableReadStream &stream) = 0;
+
+private:
+	Resources *_resources;
+};
+
+class MainDat : public Datafile {
+public:
+	MainDat(Resources *resources);
+	~MainDat();
+
+	const char *filename() const { return "iuc_main.dat"; }
+	void readFile(Common::SeekableReadStream &stream);
+
+private:
+	enum {
+		kFooterLen = 0xB6
 	};
 
+	uint16 _dataLen;
+	byte *_data;
+	byte _footer[kFooterLen];
+
+	void descramble();
 };
 
 
