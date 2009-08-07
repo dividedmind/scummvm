101099e3327b4fe8fa82fa6782d156f2d5f69f97
Rafał Rzepecki <divided.mind@gmail.com>
2009-08-07 16:05:31 +0200

    Bump debug levels of opcodes.

--- engines/innocent/opcode_handlers.cpp
+++ engines/innocent/opcode_handlers.cpp
@@ -46,20 +46,20 @@ namespace Innocent {
 
 OPCODE(0x00) {
 	// nop
-	debugC(3, kDebugLevelScript, "opcode 0x00: nop");
+	debugC(2, kDebugLevelScript, "opcode 0x00: nop");
 	return kThxBye;
 }
 
 OPCODE(0x01) {
 	// exit
 	// (some peculiarities in conj. with op 0x38, needs research TODO)
-	debugC(3, kDebugLevelScript, "opcode 0x01: exit");
+	debugC(2, kDebugLevelScript, "opcode 0x01: exit");
 	return kReturn;
 }
 
 OPCODE(0x02) {
 	// check equality
-	debugC(3, kDebugLevelScript, "opcode 0x02: if %s == %s", +a[0], +a[1]);
+	debugC(2, kDebugLevelScript, "opcode 0x02: if %s == %s", +a[0], +a[1]);
 	unless (a[0] == a[1])
 		return kFail;
 	return kThxBye;
@@ -67,7 +67,7 @@ OPCODE(0x02) {
 
 OPCODE(0x03) {
 	// check inequality
-	debugC(3, kDebugLevelScript, "opcode 0x03: if %s != %s", +a[0], +a[1]);
+	debugC(2, kDebugLevelScript, "opcode 0x03: if %s != %s", +a[0], +a[1]);
 	if (a[0] == a[1])
 		return kFail;
 	return kThxBye;
@@ -75,7 +75,7 @@ OPCODE(0x03) {
 
 OPCODE(0x04) {
 	// less than
-	debugC(3, kDebugLevelScript, "opcode 0x04: if %s < %s", +a[0], +a[1]);
+	debugC(2, kDebugLevelScript, "opcode 0x04: if %s < %s", +a[0], +a[1]);
 	unless (a[0] < a[1])
 		return kFail;
 	return kThxBye;
@@ -83,7 +83,7 @@ OPCODE(0x04) {
 
 OPCODE(0x05) {
 	// greater than
-	debugC(3, kDebugLevelScript, "opcode 0x05: if %s > %s", +a[0], +a[1]);
+	debugC(2, kDebugLevelScript, "opcode 0x05: if %s > %s", +a[0], +a[1]);
 	unless (a[0] > a[1])
 		return kFail;
 	return kThxBye;
@@ -91,7 +91,7 @@ OPCODE(0x05) {
 
 OPCODE(0x0f) {
 	// check room
-	debugC(3, kDebugLevelScript, "opcode 0x0f: if current room == %s then", +a[0]);
+	debugC(2, kDebugLevelScript, "opcode 0x0f: if current room == %s then", +a[0]);
 	unless (a[0] == _logic->currentRoom())
 		return kFail;
 	return kThxBye;
@@ -137,7 +137,7 @@ OPCODE(0x1f) {
 
 OPCODE(0x24) {
 	// check nonzeroness
-	debugC(3, kDebugLevelScript, "opcode 0x24: if (%s)", +a[0]);
+	debugC(2, kDebugLevelScript, "opcode 0x24: if (%s)", +a[0]);
 	if (a[0] == 0)
 		return kFail;
 	return kThxBye;
@@ -145,56 +145,56 @@ OPCODE(0x24) {
 
 OPCODE(0x2c) {
 	// else
-	debugC(3, kDebugLevelScript, "opcode 0x2c: else");
+	debugC(2, kDebugLevelScript, "opcode 0x2c: else");
 
 	return kElse;
 }
 
 OPCODE(0x2d) {
 	// end if
-	debugC(3, kDebugLevelScript, "opcode 0x2d: end if");
+	debugC(2, kDebugLevelScript, "opcode 0x2d: end if");
 	return kEndIf;
 }
 
 OPCODE(0x35) {
 	// jump
-	debugC(3, kDebugLevelScript, "opcode 0x35: jump to %s", +a[0]);
+	debugC(2, kDebugLevelScript, "opcode 0x35: jump to %s", +a[0]);
 	return static_cast<CodePointer &>(a[0]);
 }
 
 OPCODE(0x36) {
 	// call
-	debugC(3, kDebugLevelScript, ">>>opcode 0x36: call procedure %s", +a[0]);
+	debugC(2, kDebugLevelScript, ">>>opcode 0x36: call procedure %s", +a[0]);
 	CodePointer &p = static_cast<CodePointer &>(a[0]);
 	p.run();
-	debugC(3, kDebugLevelScript, "<<<opcode 0x36: called procedure %s", +a[0]);
+	debugC(2, kDebugLevelScript, "<<<opcode 0x36: called procedure %s", +a[0]);
 	return kReturn;
 }
 
 OPCODE(0x39) {
 	// run later
-	debugC(3, kDebugLevelScript, "opcode 0x39: execute main %s later", +a[0]);
+	debugC(2, kDebugLevelScript, "opcode 0x39: execute main %s later", +a[0]);
 	_logic->runLater(CodePointer(static_cast<CodePointer &>(a[0]).offset(), _logic->mainInterpreter()));
 	return kThxBye;
 }
 
 OPCODE(0x3b) {
 	// run local code later
-	debugC(3, kDebugLevelScript, "opcode 0x3b: execute %s later", +a[0]);
+	debugC(2, kDebugLevelScript, "opcode 0x3b: execute %s later", +a[0]);
 	_logic->runLater(CodePointer(static_cast<CodePointer &>(a[0]).offset(), _logic->blockInterpreter()));
 	return kThxBye;
 }
 
 OPCODE(0x3d) {
 	// save first arg -- instruction pointer -- for after skipping cutscene
-	debugC(3, kDebugLevelScript, "opcode 0x3d: store position to continue if cutscene skipped to %s", +a[0]);
+	debugC(2, kDebugLevelScript, "opcode 0x3d: store position to continue if cutscene skipped to %s", +a[0]);
 	Log.setSkipPoint(static_cast<CodePointer &>(a[0]));
 	return kThxBye;
 }
 
 OPCODE(0x41) {
 	// say (protagonist)
-	debugC(3, kDebugLevelScript, "opcode 0x41: say %s", +a[0]);
+	debugC(2, kDebugLevelScript, "opcode 0x41: say %s", +a[0]);
 
 	if (Log.protagonist()->isSpeaking()) {
 		Log.protagonist()->callMeWhenSilent(current);
@@ -212,7 +212,7 @@ OPCODE(0x41) {
 
 OPCODE(0x43) {
 	// say
-	debugC(3, kDebugLevelScript, "opcode 0x43: %s says %s", +a[0], +a[1]);
+	debugC(2, kDebugLevelScript, "opcode 0x43: %s says %s", +a[0], +a[1]);
 
 	Actor *ac = Log.getActor(a[0]);
 	if (ac->isSpeaking()) {
@@ -238,7 +238,7 @@ OPCODE(0x47) {
 
 OPCODE(0x4a) {
 	// wait until silent (protagonist)
-	debugC(3, kDebugLevelScript, "opcode 0x4a: wait until protagonist is silent");
+	debugC(2, kDebugLevelScript, "opcode 0x4a: wait until protagonist is silent");
 
 	Log.protagonist()->callMeWhenSilent(next);
 	return kReturn;
@@ -246,14 +246,14 @@ OPCODE(0x4a) {
 
 OPCODE(0x4b) {
 	// wait until silent
-	debugC(3, kDebugLevelScript, "opcode 0x4b: wait %s is silent", +a[0]);
+	debugC(2, kDebugLevelScript, "opcode 0x4b: wait %s is silent", +a[0]);
 
 	Log.getActor(a[0])->callMeWhenSilent(next);
 	return kReturn;
 }
 
 OPCODE(0x54) {
-	debugC(3, kDebugLevelScript, "opcode 0x54: ask about '%s' at %s:%s %sx%s", +a[4], +a[0], +a[1], +a[2], +a[3]);
+	debugC(2, kDebugLevelScript, "opcode 0x54: ask about '%s' at %s:%s %sx%s", +a[4], +a[0], +a[1], +a[2], +a[3]);
 
 	uint16 result;
 	unless ((result = _graphics->ask(a[0], a[1], a[2], a[3], a[4])) == 0xffff)
@@ -264,21 +264,21 @@ OPCODE(0x54) {
 OPCODE(0x55) {
 	// paint text
 	// args: left, top, colour, text
-	debugC(3, kDebugLevelScript, "opcode 0x55: paint '%s' with colour %s at %s:%s", +a[3], +a[2], +a[0], +a[1]);
+	debugC(2, kDebugLevelScript, "opcode 0x55: paint '%s' with colour %s at %s:%s", +a[3], +a[2], +a[0], +a[1]);
 	_graphics->paintText(a[0], a[1], a[2], a[3]);
 	return kThxBye;
 }
 
 OPCODE(0x56) {
 	// say text
-	debugC(3, kDebugLevelScript, "opcode 0x86: say %s for %s frames", +a[1], +a[0]);
+	debugC(2, kDebugLevelScript, "opcode 0x86: say %s for %s frames", +a[1], +a[0]);
 	Graf.say(a[1], a[1], a[0]);
 	return kThxBye;
 }
 
 OPCODE(0x57) {
 	// wait until said
-	debugC(3, kDebugLevelScript, "opcode 0x57: wait until text is said");
+	debugC(2, kDebugLevelScript, "opcode 0x57: wait until text is said");
 	Graf.runWhenSaid(next);
 	return kReturn;
 }
@@ -287,7 +287,7 @@ OPCODE(0x60) {
 	// lookup locally
 	// takes a list (1st)
 	// a value (2nd)
-	// a field (as offset from structure start) (3rd)
+	// a field (as offset from structure start) (2rd)
 	// first word on the list is entry length in words (minus one for index)
 	// then are entries, first word being index
 	// finds entry matching index == value in the list and
@@ -312,7 +312,7 @@ OPCODE(0x60) {
 		pos += width * 2;
 	}
 
-	debugC(3, kDebugLevelScript, "opcode 0x60: %s = %d == search list %s for %s and return field %s", +a[3], value, +a[0], +a[1], +a[2]);
+	debugC(2, kDebugLevelScript, "opcode 0x60: %s = %d == search list %s for %s and return field %s", +a[3], value, +a[0], +a[1], +a[2]);
 	a[3] = value;
 	return kThxBye;
 }
@@ -359,48 +359,48 @@ OPCODE(0x63) {
 		error("unhandled actor property %s", +a[1]);
 	}
 
-	debugC(3, kDebugLevelScript, "opcode 0x63: %s = get %s of actor %s", +a[2], desc, +a[0]);
+	debugC(2, kDebugLevelScript, "opcode 0x63: %s = get %s of actor %s", +a[2], desc, +a[0]);
 	return kThxBye;
 }
 
 OPCODE(0x6d) {
 	// increment
-	debugC(3, kDebugLevelScript, "opcode 0x6d: %s++", +a[0]);
+	debugC(2, kDebugLevelScript, "opcode 0x6d: %s++", +a[0]);
 	a[0]++;
 	return kThxBye;
 }
 
 OPCODE(0x6f) {
 	// decrement
-	debugC(3, kDebugLevelScript, "opcode 0x6d: %s--", +a[0]);
+	debugC(2, kDebugLevelScript, "opcode 0x6d: %s--", +a[0]);
 	a[0]--;
 	return kThxBye;
 }
 
 OPCODE(0x70) {
 	// assign
-	debugC(3, kDebugLevelScript, "opcode 0x70: %s = %s", +a[0], +a[1]);
+	debugC(2, kDebugLevelScript, "opcode 0x70: %s = %s", +a[0], +a[1]);
 	a[0] = a[1];
 	return kThxBye;
 }
 
 OPCODE(0x72) {
 	// assign 1
-	debugC(3, kDebugLevelScript, "opcode 0x72: %s = 1", +a[0]);
+	debugC(2, kDebugLevelScript, "opcode 0x72: %s = 1", +a[0]);
 	a[0] = 1;
 	return kThxBye;
 }
 
 OPCODE(0x73) {
 	// assign 0
-	debugC(3, kDebugLevelScript, "opcode 0x73: %s = 0", +a[0]);
+	debugC(2, kDebugLevelScript, "opcode 0x73: %s = 0", +a[0]);
 	a[0] = 0;
 	return kThxBye;
 }
 
 OPCODE(0x77) {
 	// initialize protagonist
-	debugC(3, kDebugLevelScript, "opcode 0x77: go to room %s facing %s", +a[0], +a[1]);
+	debugC(2, kDebugLevelScript, "opcode 0x77: go to room %s facing %s", +a[0], +a[1]);
 	_logic->changeRoom(a[0]);
 	_logic->protagonist()->setRoom(a[0], a[1]);
 	return kThxBye;
@@ -416,7 +416,7 @@ OPCODE(0x79) {
 OPCODE(0x7c) {
 	// toggle exit state
 	Exit *exit = _logic->blockProgram()->getExit(a[0]);
-	debugC(3, kDebugLevelScript, "opcode 0x7c: toggling exit %s to %s", +a[0], exit->isEnabled() ? "disabled" : "enabled");
+	debugC(2, kDebugLevelScript, "opcode 0x7c: toggling exit %s to %s", +a[0], exit->isEnabled() ? "disabled" : "enabled");
 	exit->setEnabled(!exit->isEnabled());
 	return kThxBye;
 }
@@ -435,7 +435,7 @@ OPCODE(0x96) {
 
 OPCODE(0x99) {
 	// wait for protagonist to exit
-	debugC(3, kDebugLevelScript, "opcoe 0x99: wait for protagonist to exit");
+	debugC(2, kDebugLevelScript, "opcoe 0x99: wait for protagonist to exit");
 
 	Actor *ac = _logic->protagonist();
 	if (ac->room() != _logic->currentRoom())
@@ -447,7 +447,7 @@ OPCODE(0x99) {
 
 OPCODE(0x9a) {
 	// wait for actor to exit
-	debugC(3, kDebugLevelScript, "opcode 0x9a: wait for actor %s to exit", +a[0]);
+	debugC(2, kDebugLevelScript, "opcode 0x9a: wait for actor %s to exit", +a[0]);
 
 	Actor *ac = _logic->getActor(a[0]);
 	if (ac->room() != _logic->currentRoom())
@@ -459,14 +459,14 @@ OPCODE(0x9a) {
 
 OPCODE(0x9b) {
 	// delay
-	debugC(3, kDebugLevelScript, "opcode 0x9b: delay %s frames", +a[0]);
+	debugC(2, kDebugLevelScript, "opcode 0x9b: delay %s frames", +a[0]);
 	_logic->runLater(next, a[0]);
 	return kReturn;
 }
 
 OPCODE(0x9c) {
 	// wait until another room
-	debugC(3, kDebugLevelScript, "opcode 0x9a: wait until actor %s enters or %s ticks", +a[0], +a[1]);
+	debugC(2, kDebugLevelScript, "opcode 0x9a: wait until actor %s enters or %s ticks", +a[0], +a[1]);
 
 	Actor *ac = _logic->getActor(a[0]);
 	if (ac->room() != _logic->currentRoom()) {
@@ -478,14 +478,14 @@ OPCODE(0x9c) {
 
 OPCODE(0x9d) {
 	// set protagonist
-	debugC(3, kDebugLevelScript, "opcode 0x9d: set protagonist(%s)", +a[0]);
+	debugC(2, kDebugLevelScript, "opcode 0x9d: set protagonist(%s)", +a[0]);
 	_logic->setProtagonist(a[0]);
 	return kThxBye;
 }
 
 OPCODE(0x9e) {
 	// warp protagonist to frame
-	debugC(3, kDebugLevelScript, "opcode 0x9e: warp protagonist to frame %s", +a[0]);
+	debugC(2, kDebugLevelScript, "opcode 0x9e: warp protagonist to frame %s", +a[0]);
 
 //	Log.protagonist()->warpTo(a[0]);
 	Log.protagonist()->setFrame(a[0]);
@@ -494,7 +494,7 @@ OPCODE(0x9e) {
 
 OPCODE(0xab) {
 	// set protagonist frame
-	debugC(3, kDebugLevelScript, "opcode 0xab: set protagonist frame to %s", +a[0]);
+	debugC(2, kDebugLevelScript, "opcode 0xab: set protagonist frame to %s", +a[0]);
 
 	Actor *ac = Log.protagonist();
 	if (ac->isFine()) {
@@ -508,7 +508,7 @@ OPCODE(0xab) {
 
 OPCODE(0xad) {
 	// turn actor
-	debugC(3, kDebugLevelScript, "opcode 0xad: move actor %s to frame %s next", +a[0], +a[1]);
+	debugC(2, kDebugLevelScript, "opcode 0xad: move actor %s to frame %s next", +a[0], +a[1]);
 
 	Actor *ac = _logic->getActor(a[0]);
 	if (ac->isFine()) {
@@ -524,7 +524,7 @@ OPCODE(0xad) {
 
 OPCODE(0xb9) {
 	// set local animation
-	debugC(3, kDebugLevelScript, "opcode 0xb9: set actor %s animation to %s", +a[0], +a[1]);
+	debugC(2, kDebugLevelScript, "opcode 0xb9: set actor %s animation to %s", +a[0], +a[1]);
 
 	Actor *ac = Log.getActor(a[0]);
 	if (ac->isFine()) {
@@ -538,7 +538,7 @@ OPCODE(0xb9) {
 
 OPCODE(0xbc) {
 	// hide actor
-	debugC(3, kDebugLevelScript, "opcode 0xbc: hide actor %s", +a[0]);
+	debugC(2, kDebugLevelScript, "opcode 0xbc: hide actor %s", +a[0]);
 	_logic->getActor(a[0])->hide();
 	return kThxBye;
 }
@@ -546,7 +546,7 @@ OPCODE(0xbc) {
 OPCODE(0xbd) {
 	// set protagonist animation
 	CodePointer p(static_cast<CodePointer &>(a[0]).offset(), Log.mainInterpreter());
-	debugC(3, kDebugLevelScript, "opcode 0xbd: set protagonist animation to %s", +p);
+	debugC(2, kDebugLevelScript, "opcode 0xbd: set protagonist animation to %s", +p);
 
 	Actor *ac = Log.protagonist();
 	if (ac->isFine()) {
@@ -560,7 +560,7 @@ OPCODE(0xbd) {
 
 OPCODE(0xbe) {
 	// set protagonist animation
-	debugC(3, kDebugLevelScript, "opcode 0xbe: set protagonist animation to %s", +a[0]);
+	debugC(2, kDebugLevelScript, "opcode 0xbe: set protagonist animation to %s", +a[0]);
 
 	Actor *ac = Log.protagonist();
 	if (ac->isFine()) {
@@ -574,21 +574,21 @@ OPCODE(0xbe) {
 
 OPCODE(0xc2) {
 	// add animation at cursor
-	debugC(3, kDebugLevelScript, "opcode 0xc2: add animation %s at cursor partial STUB", +a[0]);
+	debugC(2, kDebugLevelScript, "opcode 0xc2: add animation %s at cursor partial STUB", +a[0]);
 	_logic->addAnimation(new Animation(static_cast<CodePointer &>(a[0]), _graphics->cursorPosition()));
 	return kThxBye;
 }
 
 OPCODE(0xc6) {
 	// suspend execution until an animation's ip points to 0xff
-	debugC(3, kDebugLevelScript, "opcode 0xc6: wait on animation %s", +a[0]);
+	debugC(2, kDebugLevelScript, "opcode 0xc6: wait on animation %s", +a[0]);
 	_logic->animation(a[0])->runOnNextFrame(next);
 	return kReturn;
 }
 
 OPCODE(0xc7) {
 	// play movie
-	debugC(3, kDebugLevelScript, "opcode 0xc7: play movie %s with slowness %s", +a[0], +a[1]);
+	debugC(2, kDebugLevelScript, "opcode 0xc7: play movie %s with slowness %s", +a[0], +a[1]);
 	Movie *m = Movie::fromFile(reinterpret_cast<char *>((byte *)(a[0])));
 	m->setFrameDelay(a[1]);
 	if (m->play())
@@ -600,7 +600,7 @@ OPCODE(0xc7) {
 OPCODE(0xc8) {
 	// set backdrop
 	// (not sure what's the difference to c9)
-	debugC(3, kDebugLevelScript, "opcode 0xc8: set backdrop(%s)", +a[0]);
+	debugC(2, kDebugLevelScript, "opcode 0xc8: set backdrop(%s)", +a[0]);
 	_graphics->setBackdrop(a[0]);
 	return kThxBye;
 }
@@ -608,7 +608,7 @@ OPCODE(0xc8) {
 OPCODE(0xc9) {
 	// set backdrop
 	// does it set the default one?
-	debugC(3, kDebugLevelScript, "opcode 0xc9: set backdrop(%s)", +a[0]);
+	debugC(2, kDebugLevelScript, "opcode 0xc9: set backdrop(%s)", +a[0]);
 	_graphics->setBackdrop(a[0]);
 	return kThxBye;
 }
@@ -653,13 +653,13 @@ OPCODE(0xd0) {
 }
 
 OPCODE(0xd1) {
-	debugC(3, kDebugLevelScript, "opcode 0xd1: fadein next paint");
+	debugC(2, kDebugLevelScript, "opcode 0xd1: fadein next paint");
 	_graphics->willFadein();
 	return kThxBye;
 }
 
 OPCODE(0xd2) {
-	debugC(3, kDebugLevelScript, "opcode 0xd2: will fadein partially");
+	debugC(2, kDebugLevelScript, "opcode 0xd2: will fadein partially");
 	Graf.willFadein(Graphics::kPartialFade);
 	return kThxBye;
 }
@@ -702,7 +702,7 @@ OPCODE(0xdf) {
 	}
 	const int16 left = a[0].signd();
 	const int16 top = a[1].signd();
-	debugC(3, kDebugLevelScript, "opcode 0xdf: add actor frame %d %d %d %d %d %d %d %d %d %d", left, top, nexts[0], nexts[1], nexts[2], nexts[3], nexts[4], nexts[5], nexts[6], nexts[7]);
+	debugC(2, kDebugLevelScript, "opcode 0xdf: add actor frame %d %d %d %d %d %d %d %d %d %d", left, top, nexts[0], nexts[1], nexts[2], nexts[3], nexts[4], nexts[5], nexts[6], nexts[7]);
 	Log.room()->addActorFrame(Common::Point(left, top), nexts);
 	return kThxBye;
 }
@@ -715,7 +715,7 @@ OPCODE(0xe5) {
 
 OPCODE(0xe6) {
 	// set room loop code
-	debugC(3, kDebugLevelScript, "opcode 0xe6: set room loop to %s", +a[0]);
+	debugC(2, kDebugLevelScript, "opcode 0xe6: set room loop to %s", +a[0]);
 	assert(a[0].holdsCode());
 	_logic->setRoomLoop(static_cast<CodePointer &>(a[0]));
 	return kThxBye;
@@ -724,7 +724,7 @@ OPCODE(0xe6) {
 OPCODE(0xef) {
 	// random
 	uint16 value = _engine->getRandom(a[0]);
-	debugC(3, kDebugLevelScript, "opcode 0xef: %s = %d == random(%s)", +a[1], value, +a[0]);
+	debugC(2, kDebugLevelScript, "opcode 0xef: %s = %d == random(%s)", +a[1], value, +a[0]);
 	a[1] = value;
 	return kThxBye;
 }
@@ -744,7 +744,7 @@ OPCODE(0xf4) {
 
 OPCODE(0xf7) {
 	// stop music
-	debugC(3, kDebugLevelScript, "opcode 0xf7: stop music");
+	debugC(2, kDebugLevelScript, "opcode 0xf7: stop music");
 	Music.unloadMusic();
 	Music.silence();
 	return kThxBye;
@@ -759,7 +759,7 @@ OPCODE(0xf9) {
 
 OPCODE(0xfc) {
 	// quit
-	debugC(3, kDebugLevelScript, "opcode 0xfc: quit%s", a[0] == 0 ? "" : " unconditionally");
+	debugC(2, kDebugLevelScript, "opcode 0xfc: quit%s", a[0] == 0 ? "" : " unconditionally");
 	if (a[0] == 0)
 		error("asking for quitting not implemented");
 
